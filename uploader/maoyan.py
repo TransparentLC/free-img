f132e52f39a32a0100ba0779257f53fe0175198e
✨小透明・宸✨ <1272742391@qq.com>
2021-11-05 15:58:24 +0800

    Update maoyan.py

--- uploader/maoyan.py
+++ uploader/maoyan.py
@@ -5,7 +5,7 @@ from uploader import AbstractUploader
 class Uploader(AbstractUploader):
     @property
     def request_url(self) -> str:
-        return 'https://maoyan.com/ajax/proxy/admin/mmdb/photos/upload.json'
+        return 'https://www.maoyan.com/ajax/proxy/admin/mmdb/photos/upload.json'
 
     @property
     def file_key(self) -> str:
@@ -13,7 +13,7 @@ class Uploader(AbstractUploader):
 
     @property
     def parsed(self) -> str:
-        return self.request.json()['data'][0]['olink'].replace('http://', 'https://')
+        return self.request.json()['data'][0]['olink'].replace('?imageMogr2/thumbnail/2500x2500%3E', '?imageMogr2/')
 
     def filename_rewrite(self, filename: str) -> str:
         if os.path.splitext(filename.lower())[1] in {'.webp', '.avif'}:
